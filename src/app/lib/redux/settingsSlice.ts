bb8a26cbac05db6b142b52e9776cdf863b1c79d1
Waqar Shakeel <waqarshakeel29@gmail.com>
2024-08-27 03:30:31 +0500

    pdf viewer off by default

--- src/app/lib/redux/settingsSlice.ts
+++ src/app/lib/redux/settingsSlice.ts
@@ -41,7 +41,7 @@ export const DEFAULT_THEME_COLOR = "#DF6951"; // sky-400
 export const DEFAULT_FONT_FAMILY = "Roboto";
 export const DEFAULT_FONT_SIZE = "11"; // text-base https://tailwindcss.com/docs/font-size
 export const DEFAULT_FONT_COLOR = "#171717"; // text-neutral-800
-export const DEFAULT_PDF_VIEWER = "YES";
+export const DEFAULT_PDF_VIEWER = "NO";
 
 export const initialSettings: Settings = {
   themeColor: DEFAULT_THEME_COLOR,
